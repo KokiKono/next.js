8f0325e61b67d8dfc3e534946b20ce7967d4f67d
河野浩生 <61747641koki@gmail.com>
2020-10-10 12:12:29 +0900

    create githubactions and lint and lint:fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link';
+import React from "react";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -12,11 +12,14 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Read <Link href="/posts/first-post"><a>this page!</a></Link>
+          Read{" "}
+          <Link href="/posts/first-post">
+            <a>this page!</a>
+          </Link>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -57,10 +60,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
